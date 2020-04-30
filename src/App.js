0a774d7c775371c6192147df33b3adbaf80cbdc5
Dan Atkinson <dan@danatkinson.com>
2020-04-30 09:50:54 +0200

    Add start game function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Start from './components/Start';
+import { GAMESTATE } from './constants';
+
+import Start from "./components/Start";
 
 function App() {
+  const [player1, setplayer1] = useState("");
+  const [player2, setplayer2] = useState("");
+  const [gameState, setGameState] = useState(GAMESTATE.waiting);
+
+  const startGame = (player1Name, player2Name) => {
+    console.log('start game');
+    setplayer1(player1Name);
+    setplayer2(player2Name);
+    setGameState(GAMESTATE.started);
+  }
+
   return (
     <div className="App">
       <h1>Tic Tac Toe</h1>
-      <Start/>
-
+      <Start startGame={startGame} show={gameState === GAMESTATE.waiting} />
     </div>
   );
 }
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,29 +1,36 @@
-import React, { useState} from 'react'
+import React, { useState } from "react";
 
-import Button from './Button';
-import './Start.css';
+import Button from "./Button";
+import "./Start.css";
 
+export default function Start({ show, startGame }) {
+  const [player1, setPlayer1] = useState("Player 1");
+  const [player2, setPlayer2] = useState("Player 2");
 
-export default function Start() {
-  const [player1, setPlayer1] = useState('Player 1');
-  const [player2, setPlayer2] = useState('Player 2');
+  if (!show) {
+    return null;
+  }
+
+  const clickStartGame = () => {
+    startGame(player1, player2);
+  };
 
   return (
     <div className="Start">
       <form>
-      <h2>Enter Game Details</h2>
-      <input
+        <h2>Enter Game Details</h2>
+        <input
           type="text"
           value={player1}
-          onChange={e => setPlayer1(e.target.value)}
-      />
-      <input 
+          onChange={(e) => setPlayer1(e.target.value)}
+        />
+        <input
           type="text"
           value={player2}
-          onChange={e => setPlayer2(e.target.value)}
-      />
-      <Button title="Start Game!"/>
+          onChange={(e) => setPlayer2(e.target.value)}
+        />
+        <Button onClick={clickStartGame} title="Start Game!" />
       </form>
     </div>
-  )
+  );
 }
