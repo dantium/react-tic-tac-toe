cb06111e4ab92de3833b20571c928467a2303a6a
Dan Atkinson <dan@danatkinson.com>
2020-04-30 15:27:02 +0200

    Update board and tile styles, add SVGs for tile symbols

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import {ReactComponent as CrossSVG} from '../cross.svg';
+import {ReactComponent as CircleSVG} from '../circle.svg';
 import "./Tile.css";
 
 function Symbol({ value }) {
-  return value === 0 ? null : value === 1 ? "X" : "O";
+  return (value === 0) ? null : (value === 1) ? <CrossSVG/> : <CircleSVG/>;
 }
 
 export default function Tile({ tile, index, rindex, clickTile }) {
