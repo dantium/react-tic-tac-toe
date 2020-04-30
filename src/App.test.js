b8d17b856eac617044ecdac029eef2b948e349a2
Dan Atkinson <dan@danatkinson.com>
2020-04-30 09:23:48 +0200

    Clean up create-react-app defaults, remove serviceWorker and logos

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headElement = getByText('Tic Tac Toe');
+  expect(headElement).toBeInTheDocument();
 });
